94b5044ef1b6eabcffee9927a2791ba28ef704b6
R26 <95674004+R26AST@users.noreply.github.com>
2024-06-23 13:45:06 +0700

    Add files via upload

---
+++ assets/js/page.js
@@ -0,0 +1,11 @@
+$('.tahun').append(new Date().getFullYear());
+
+/* Tombol kembali */
+
+$('.home').click(function() {
+	halaman('home');
+});
+
+$('.btn-back').click(function() {
+	halaman('home');
+});
