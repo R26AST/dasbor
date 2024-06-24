4a9d843c305e36b501e33491dcb00a5bc688fed7
R26 <95674004+R26AST@users.noreply.github.com>
2024-06-24 14:11:05 +0700

    Update page.js

--- assets/js/page.js
+++ assets/js/page.js
@@ -29,3 +29,60 @@ function waktu() {
 }
 
 var inter = setInterval(waktu, 1000);
+
+
+/*tes jaringan server */
+
+var time_start, end_time, mode = 1;
+var imageLink = 'https://upload.wikimedia.org/wikipedia/commons/3/3e/Tokyo_Sky_Tree_2012.JPG';
+// The size in bytes
+var downloadSize = 8185374;
+var downloadImgSrc = new Image();
+
+$('.test-jaringan').click(function() {
+	time_start = new Date().getTime();
+	//downloadImgSrc.src = imageLink;
+	var cacheImg = "?nn=" + time_start;
+	downloadImgSrc.src = imageLink + cacheImg;
+	downloadImgSrc.onload = function () {
+		end_time = new Date().getTime();
+		displaySpeed();
+	};
+});
+
+function displaySpeed() {
+    var timeDuration = (end_time - time_start) / 1000;
+    var loadedBits = downloadSize * 8;
+    /* Converts a number into string
+    using toFixed(2) rounding to 2 */
+    var bps = (loadedBits / timeDuration);
+    var speedInKbps = (bps / 1024).toFixed(2);
+	var speedInMbps = (bps / 1024 / 1024).toFixed(2);
+	
+    if(speedInKbps > 800)
+    {
+        cls = 'text-success';
+        cls_txt = 'Good';
+    }
+    else if(speedInKbps > 400)
+    {
+        cls = 'text-warning';
+        cls_txt = 'Moderate';
+    }
+    else
+    {
+        cls = 'text-danger';
+        cls_txt = 'Bad';
+    }
+
+    if(mode == 1)
+    {
+        $('#network_status').html('<i class="fa fa-signal '+cls+'"></i> '+cls_txt);
+        $('#network_speed').html(speedInMbps+" Mbps");
+    }
+    else if(mode == 2)
+    {
+        $.get(URL_SAVE_NETWORK_INFO+'/'+speedInKbps+'/'+cls_txt);
+    }
+}
+
